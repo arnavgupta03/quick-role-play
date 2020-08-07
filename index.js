ab3ff080ada5d43de3f125b63acd23b2a1328550
arnavgupta03 <arnavgupt03@gmail.com>
2020-08-07 10:38:59 -0400

    Fix button functions with clears and removes

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 function startAdvOne(){
+    localStorage.clear();
     localStorage.setItem("adv", "1");
 }
 function startAdvTwo() {
+    localStorage.clear();
     localStorage.setItem("adv", "2");
 }
 function startAdvThree() {
+    localStorage.clear();
     localStorage.setItem("adv", "3");
 }
 function playersOne() {
+    localStorage.removeItem("players");
     localStorage.setItem("players", "1");
 }
 function playersTwo() {
+    localStorage.removeItem("players");
     localStorage.setItem("players", "2");
 }
