dff84fab8b6cb2088ca023e02ca10d2a1eb4e55d
arnavgupta03 <arnavgupt03@gmail.com>
2020-08-07 18:03:12 -0400

    Added features to prompt 1 for washroom

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ function playersTwo() {
     window.history.pushState("","","index.html");
 }
 function findAdventure() {
-    advChose = localStorage.getItem("adventure");
-    players = localStorage.getItem("players");
-    player1name = document.getElementById("player-one").value;
+    var advChose = localStorage.getItem("adventure");
+    var players = localStorage.getItem("players");
+    var player1name = document.getElementById("player-one").value;
     if (advChose === "1"){
         startAdvOne(players, player1name);
     } else if (advChose === "2"){
@@ -50,29 +50,58 @@ function findAdventure() {
 function startAdvOne(numplay, player1name) {
     if (numplay === "1"){
         if(player1name === ""){
-            document.getElementById("playerprompt").innerHTML = "Welcome Adventurer! Let's begin.<br><button id='startgame' onclick='prompt1()'>Start Game</button>";
+            document.getElementById("playerprompt").innerHTML = "Welcome Adventurer! Let's begin.<br><button id='startgame' onclick='prompt1AdvOne()'>Start Game</button>";
         } else if(player1name !== ""){
-            document.getElementById("playerprompt").innerHTML = "Welcome " + player1name + "! Let's begin.<br><button id='startgame' onclick='prompt1()'>Start Game</button>";
+            document.getElementById("playerprompt").innerHTML = "Welcome " + player1name + "! Let's begin.<br><button id='startgame' onclick='prompt1AdvOne()'>Start Game</button>";
         } else {
             document.getElementById("playerprompt").innerHTML = "Oh no, some Dark Elf must've caused an error! Please reload page."
         }
     }
 }
-function prompt1() {
+function prompt1AdvOne() {
     var startprompt = document.createElement("p");
     startprompt.innerHTML = "You are a worker for Greedy Ltd, the world's largest, and now only, manufacturer of intelligent robots. These robots seem to have taken over the world, making sure people don't have to do anything. Only problem with that is that now people don't have any jobs. You're actually one of the lucky few who got a job with Greedy Ltd. So yeah, things aren't going great. That is until one day, when you hear that the owner of Greedy Ltd has died, without an heir. The company will be passed onto another important person in the company. Unless you can get the code to control and shut down all the robots and save the world. Only problem is that you'll need to do it before the robots, controlled by Greedy Ltd's CTO, get the code. Good luck!";
     startprompt.style.width = "98vw";
     startprompt.style.position = "absolute";
     var firstquest = document.createElement("p");
-    firstquest.innerHTML = "You're packing your stuff at your desk when the idea to get the code just comes to you. But there's not really much you can do. At least right now. The only places you can go are the hallway to management, the hallway outside, and the washroom. Where do you want to go?<br>";
+    firstquest.innerHTML = "You're packing your stuff at your desk when the idea to get the code just comes to you. But there's not really much you can do. At least right now. The only places you can go are the hallway to management, the hallway outside, and the washroom. Where do you want to go?<br><br>";
     firstquest.style.width = "98vw";
     firstquest.style.position = "absolute";
+    var startpromptlabel = document.createElement("label");
+    startpromptlabel.htmlFor = "startresponse";
+    startpromptlabel.innerHTML = "What do you want to do?";
     var startpromptinp = document.createElement("input");
     startpromptinp.type = "text";
     startpromptinp.width = "98vw";
-    firstquest.appendChild(startpromptinp);
+    startpromptinp.id = "startresponse";
+    startpromptinp.placeholder = "Say Something...";
+    /*var startpromptbut = document.createElement("button");
+    startpromptbut.onclick = "checkPrompt1AdvOne()";
+    startpromptbut.innerHTML = "";
+    startpromptinp.appendChild(startpromptbut);*/
+    startpromptlabel.appendChild(startpromptinp);
+    firstquest.appendChild(startpromptlabel);
     startprompt.appendChild(firstquest);
     document.getElementById("play").appendChild(startprompt);
+    const inputField = document.getElementById("startresponse");
+    inputField.addEventListener("keydown", function(e) {
+        if (e.code === "Enter"){
+            var input = inputField.value;
+            inputField.value = "";
+            checkPrompt1AdvOne(input);
+        }
+    });
+}
+function checkPrompt1AdvOne(choice) {
+    if (choice.toLowerCase().includes("washroom")){
+        alert("included washroom");
+        prompt2WashroomAdvOne();
+    }
+}
+function prompt2WashroomAdvOne() {
+    var response = document.createElement("p");
+    response.innerHTML = "Great! You go to the washroom.";
+    document.getElementById("play").appendChild(response);
 }
 function startAdvTwo(numplay, play1name) {
     
