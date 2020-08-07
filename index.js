3971fb7ca6c922811268b9bacf9d5f0be3d7e11c
arnavgupta03 <arnavgupt03@gmail.com>
2020-08-07 16:32:08 -0400

    Change url function and add prompt for adventure 1

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-function chooseAdv() {
+function onLoad(){
+    window.location.assign("#header");
+    window.history.pushState("","","index.html");
+}
+function chooseAdv(){
     window.location.assign("#choose-adv");
+    window.history.pushState("","","index.html");
 }
 function choseAdvOne(){
     localStorage.setItem("adventure", "1");
     window.location.assign("#num-players");
+    window.history.pushState("","","index.html");
 }
 function choseAdvTwo() {
     localStorage.setItem("adventure", "2");
     window.location.assign("#num-players");
+    window.history.pushState("","","index.html");
 }
 function choseAdvThree() {
     localStorage.setItem("adventure", "3");
     window.location.assign("#num-players");
+    window.history.pushState("","","index.html");
+}
+function playersOne() {
+    localStorage.removeItem("players");
+    localStorage.setItem("players", "1");
+    findAdventure();
+    window.location.assign("#play");
+    window.history.pushState("","","index.html");
+}
+function playersTwo() {
+    localStorage.removeItem("players");
+    localStorage.setItem("players", "2");
+    findAdventure();
+    window.location.assign("#play");
+    window.history.pushState("","","index.html");
 }
 function findAdventure() {
     advChose = localStorage.getItem("adventure");
@@ -25,24 +47,32 @@ function findAdventure() {
         startAdvThree(players, player1name);
     }
 }
-function playersOne() {
-    localStorage.removeItem("players");
-    localStorage.setItem("players", "1");
-    findAdventure();
-    window.location.assign("#play");
-}
-function playersTwo() {
-    localStorage.removeItem("players");
-    localStorage.setItem("players", "2");
-    findAdventure();
-    window.location.assign("#play");
-}
 function startAdvOne(numplay, player1name) {
-    if(player1name === ""){
-        document.getElementById("playerprompt").innerHTML = "Welcome Adventurer! Let's begin.";
-    } else if(player1name !== ""){
-        document.getElementById("playerprompt").innerHTML = "Welcome " + player1name + "! Let's begin.";
+    if (numplay === "1"){
+        if(player1name === ""){
+            document.getElementById("playerprompt").innerHTML = "Welcome Adventurer! Let's begin.<br><button id='startgame' onclick='prompt1()'>Start Game</button>";
+        } else if(player1name !== ""){
+            document.getElementById("playerprompt").innerHTML = "Welcome " + player1name + "! Let's begin.<br><button id='startgame' onclick='prompt1()'>Start Game</button>";
+        } else {
+            document.getElementById("playerprompt").innerHTML = "Oh no, some Dark Elf must've caused an error! Please reload page."
+        }
     }
+}
+function prompt1() {
+    var startprompt = document.createElement("p");
+    startprompt.innerHTML = "You are a worker for Greedy Ltd, the world's largest, and now only, manufacturer of intelligent robots. These robots seem to have taken over the world, making sure people don't have to do anything. Only problem with that is that now people don't have any jobs. You're actually one of the lucky few who got a job with Greedy Ltd. So yeah, things aren't going great. That is until one day, when you hear that the owner of Greedy Ltd has died, without an heir. The company will be passed onto another important person in the company. Unless you can get the code to control and shut down all the robots and save the world. Only problem is that you'll need to do it before the robots, controlled by Greedy Ltd's CTO, get the code. Good luck!";
+    startprompt.style.width = "98vw";
+    startprompt.style.position = "absolute";
+    var firstquest = document.createElement("p");
+    firstquest.innerHTML = "You're packing your stuff at your desk when the idea to get the code just comes to you. But there's not really much you can do. At least right now. The only places you can go are the hallway to management, the hallway outside, and the washroom. Where do you want to go?<br>";
+    firstquest.style.width = "98vw";
+    firstquest.style.position = "absolute";
+    var startpromptinp = document.createElement("input");
+    startpromptinp.type = "text";
+    startpromptinp.width = "98vw";
+    firstquest.appendChild(startpromptinp);
+    startprompt.appendChild(firstquest);
+    document.getElementById("play").appendChild(startprompt);
 }
 function startAdvTwo(numplay, play1name) {
     
