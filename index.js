a032aa6d44c2c6acbc4f2a6dbc28c55295bfb491
arnavgupta03 <arnavgupt03@gmail.com>
2020-08-07 10:33:54 -0400

    Fix adventure and players functions

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function startAdvOne(){
-
+    localStorage.setItem("adv", "1");
 }
 function startAdvTwo() {
-    
+    localStorage.setItem("adv", "2");
 }
 function startAdvThree() {
-    
+    localStorage.setItem("adv", "3");
 }
 function playersOne() {
-    
+    localStorage.setItem("players", "1");
 }
 function playersTwo() {
-    
+    localStorage.setItem("players", "2");
 }
