f62ad9512417c87a862aae7690046cf889c78699
arnavgupta03 <arnavgupt03@gmail.com>
2020-08-07 10:48:21 -0400

    Allow buttons to change location

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 function startAdvOne(){
     localStorage.clear();
     localStorage.setItem("adv", "1");
+    window.location.assign("#num-players");
 }
 function startAdvTwo() {
     localStorage.clear();
     localStorage.setItem("adv", "2");
+    window.location.assign("#num-players");
 }
 function startAdvThree() {
     localStorage.clear();
     localStorage.setItem("adv", "3");
+    window.location.assign("#num-players");
 }
 function playersOne() {
     localStorage.removeItem("players");
     localStorage.setItem("players", "1");
+    window.location.assign("#play");
 }
 function playersTwo() {
     localStorage.removeItem("players");
     localStorage.setItem("players", "2");
+    window.location.assign("#play");
 }
