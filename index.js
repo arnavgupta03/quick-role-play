4f26fc7f5ffdb80ee74c7ea94b735920ea0cfdfc
arnavgupta03 <arnavgupt03@gmail.com>
2020-08-07 13:07:00 -0400

    Allow games to start for each adventure

--- index.js
+++ index.js
@@ -16,13 +16,35 @@ function startAdvThree() {
     localStorage.setItem("adv", "3");
     window.location.assign("#num-players");
 }
+function findAdventure() {
+    advChose = localStorage.getItem("adv");
+    players = localStorage.getItem("players");
+    if (advChose === "1"){
+        startAdvOne(players);
+    } else if (advChose === "2"){
+        startAdvTwo(players);
+    } else if (advChose === "3"){
+        startAdvThree(players);
+    }
+}
 function playersOne() {
     localStorage.removeItem("players");
     localStorage.setItem("players", "1");
+    findAdventure();
     window.location.assign("#play");
 }
 function playersTwo() {
     localStorage.removeItem("players");
     localStorage.setItem("players", "2");
+    findAdventure();
     window.location.assign("#play");
+}
+function startAdvOne(numplay) {
+    
+}
+function startAdvTwo(numplay) {
+    
+}
+function startAdvThree(numplay) {
+    
 }
